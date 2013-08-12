025ccdb657ab56535bb1e1452ce3e87f74b8d1cc
Toby Inkster <tobyink@cpan.org>
2013-08-12 21:33:07 +0000

    Import of TOBYINK/RDF-DOAP-0.001 from CPAN.
    gitpan-cpan-distribution: RDF-DOAP
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         TOBYINK/RDF-DOAP-0.001.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

---
+++ lib/RDF/DOAP/Person.pm
@@ -0,0 +1,94 @@
+package RDF::DOAP::Person;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = 0.001;
+
+use Moose;
+extends qw(RDF::DOAP::Resource);
+
+use RDF::DOAP::Types -types;
+use RDF::DOAP::Utils -traits;
+
+use RDF::Trine::Namespace qw(rdf rdfs owl xsd);
+my $foaf = 'RDF::Trine::Namespace'->new('http://xmlns.com/foaf/0.1/');
+
+has $_ => (
+	traits     => [ WithURI ],
+	is         => 'ro',
+	isa        => String,
+	coerce     => 1,
+	uri        => $foaf->$_,
+) for qw( name nick );
+
+has mbox => (
+	traits     => [ WithURI ],
+	is         => 'ro',
+	isa        => ArrayRef[Identifier],
+	coerce     => 1,
+	uri        => $foaf->mbox,
+	multi      => 1,
+);
+
+has cpanid => (
+	is         => 'ro',
+	isa        => Maybe[Str],
+	lazy       => 1,
+	builder    => '_build_cpanid',
+);
+
+sub _build_cpanid
+{
+	my $self = shift;
+	return unless $self->has_rdf_about;
+	$self->rdf_about =~ m{^http://purl\.org/NET/cpan-uri/person/(\w+)$}
+		and return $1;
+	return;
+}
+
+sub to_string
+{
+	my $self = shift;
+	
+	return $self->name
+		|| $self->nick
+		|| $self->cpanid
+		|| ($self->mbox && $self->mbox->[0]->uri)
+		|| ($self->rdf_about && $self->rdf_about->uri)
+		|| 'Anonymous'
+		if @_ && $_[0] eq 'compact';
+	
+	my @parts;
+	if ($self->name)
+	{
+		push @parts, $self->name;
+		if ($self->cpanid)
+		{
+			push @parts, sprintf('(cpan:%s)', uc $self->cpanid);
+		}
+	}
+	else
+	{
+		my $nick = uc($self->nick) || uc($self->cpanid);
+		push @parts, $nick if $nick;
+	}
+	
+	for my $mbox (@{$self->mbox || []})
+	{
+		if ($mbox and $mbox->uri =~ /^mailto:(.+)$/)
+		{
+			push @parts, "<$1>";
+			last;
+		}
+	}
+	
+	for my $mbox (@{$self->mbox || []})
+	{
+		push @parts, $mbox if !@parts;
+	}
+	
+	push @parts, $self->rdf_about if !@parts && $self->has_rdf_about;
+	
+	join(" ", grep !!$_, @parts) or 'Anonymous';
+}
+
+1;
---
+++ lib/RDF/DOAP/Version.pm
@@ -0,0 +1,93 @@
+package RDF::DOAP::Version;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = 0.001;
+
+use Moose;
+extends qw(RDF::DOAP::Resource);
+
+use RDF::DOAP::ChangeSet;
+use RDF::DOAP::Change;
+use RDF::DOAP::Person;
+use RDF::DOAP::Types -types;
+use RDF::DOAP::Utils -traits;
+
+use RDF::Trine::Namespace qw(rdf rdfs owl xsd);
+my $doap = 'RDF::Trine::Namespace'->new('http://usefulinc.com/ns/doap#');
+my $dc   = 'RDF::Trine::Namespace'->new('http://purl.org/dc/terms/');
+my $dcs  = 'RDF::Trine::Namespace'->new('http://ontologi.es/doap-changeset#');
+
+has $_ => (
+	traits     => [ WithURI ],
+	is         => 'ro',
+	isa        => String,
+	coerce     => 1,
+	uri        => $doap->$_,
+) for qw( revision name branch );
+
+has issued => (
+	traits     => [ WithURI ],
+	is         => 'ro',
+	isa        => String,
+	coerce     => 1,
+	uri        => $dc->issued,
+);
+
+has changesets => (
+	traits     => [ WithURI ],
+	is         => 'ro',
+	isa        => ArrayRef[ChangeSet],
+	coerce     => 1,
+	uri        => $dcs->changeset,
+	multi      => 1,
+);
+
+has changes => (
+	is         => 'ro',
+	isa        => ArrayRef[Change],
+	coerce     => 1,
+	lazy       => 1,
+	builder    => '_build_changes',
+);
+
+has released_by => (
+	traits     => [ WithURI, Gathering ],
+	is         => 'ro',
+	isa        => Person,
+	coerce     => 1,
+	uri        => $dcs->uri('released-by'),
+	gather_as  => ['maintainer'],
+);
+
+sub _build_changes
+{
+	my $self = shift;
+	[ map { @{$_->items} } @{$self->changesets || []} ];
+}
+
+sub changelog_section
+{
+	my $self = shift;
+	
+	return join(
+		"\n",
+		$self->_changelog_section_header,
+		sort map $_->changelog_entry, @{$self->changes},
+	)."\n";
+}
+
+sub _changelog_section_header
+{
+	my $self = shift;
+	return join(
+		"\t",
+		grep(
+			defined,
+			$self->revision,
+			($self->issued // 'Unknown'),
+			($self->name // $self->label),
+		),
+	) . "\n";
+}
+
+1;
